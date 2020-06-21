5e735c85b370f55069bb284bcbaa5c1089879ea9
Annie Huang <annie.hn@gmail.com>
2020-06-21 23:07:55 +1000

    Implemented the same logic from selecting parent then selecting child within the parent.

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,14 +1,28 @@
 //using selectors inside the element
-// traversing the dom
+const questions = document.querySelectorAll('.question');
 
+questions.forEach(question => {
+    console.log(question);
+    const btn = question.querySelector('.question-btn')
+    console.log(btn);
+
+    btn.addEventListener('click', () => {
+        question.classList.toggle('show-text');
+    });
+})
+
+
+/*
+// traversing the dom
 const btns = document.querySelectorAll('.question-btn');
 
 btns.forEach(btn => {
     btn.addEventListener('click', (e) => {
         console.log(e.currentTarget); // <button type="button" class="question-btn">..</button>
-        console.log(e.currentTarget.parentElement);
+        console.log(e.currentTarget.parentElement); <article class="question">...</article>
 
         const question = e.currentTarget.parentElement.parentElement;
         question.classList.toggle('show-text');
     });
 });
+*/
