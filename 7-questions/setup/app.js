3289e6a12e474f0c1ec0a41d4194292ae54610ea
Annie Huang <annie.hn@gmail.com>
2020-06-21 22:56:46 +1000

    Implement the open and closing accordion by traversing the dom (e.currentTarget.parentElement.parentElement)

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,14 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.currentTarget); // <button type="button" class="question-btn">..</button>
+        console.log(e.currentTarget.parentElement);
+
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
+    });
+});
