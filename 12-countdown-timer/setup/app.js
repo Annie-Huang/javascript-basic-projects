4d9b942847b1b5ec9411c4b08bf4e4bb4ea9c45c
Annie Huang <annie.hn@gmail.com>
2020-07-04 19:00:14 +1000

    Set up to show the correct content for .giveaway (if it's a fix date, wouldn't you just hardcode it? haha)

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,65 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+// console.log(items);
+
+// let futureDate = new Date();
+
+// Mon May 25 2020 11:30:00 GMT+1000 (Australian Eastern Standard Time)
+// month start from 0, other fields are matching the number.
+let futureDate = new Date(2020, 4, 25, 11,30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours  = futureDate.getHours();
+const minutes  = futureDate.getMinutes();
+const month  = months[futureDate.getMonth()];
+const date  = futureDate.getDate();
+const weekday  = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
